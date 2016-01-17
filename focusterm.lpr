e80034949d061668c69fb9b59cc3ee209e9e3fdd
Daniel Bailey <danieljabailey@gmail.com>
2016-01-17 18:42:08 +0000

    First reasonable working version

--- focusterm.lpr
+++ focusterm.lpr
@@ -8,14 +8,63 @@ uses
   {$ENDIF}
   Classes,
   { you can add units after this }
-  XFocusListenerThread, xlib, sysutils;
+  XFocusListenerThread, xlib, sysutils, dateutils,
+  sdposerial, math, IniFiles;
+
+type
+  TserialHandler = class(TObject)
+    public
+      procedure gotSerial(obj: TObject);
+  end;
 
 var
   FocusThread: TXFocusListenerThread;
   XDisplay: PDisplay;
+  serial: TSdpoSerial;
+  serialHandler: TserialHandler;
+  conf: TINIFile;
+
+  portName: string;
+  focusColour: string;
+  otherColour: string;
+  fo_r_h, fo_g_h, fo_b_h, fo_r_l, fo_g_l, fo_b_l: char;
+  ot_r_h, ot_g_h, ot_b_h, ot_r_l, ot_g_l, ot_b_l: char;
+  l_skip, l_light, l_tot: integer;
+  ls_l, ls_h, ll_l, ll_h, lt_l, lt_h : char;
+  gi: integer;
+
+
+const
+  iniFileName = 'focusbar.conf';
+
+function isLit(i, numLEDs: integer):boolean;
+var relPos, relStart, relEnd: extended;
+begin
+  relPos := i/numLEDs;
+  relStart := max(0,FocusThread.CurrentRect.Left) / FocusThread.RootRect.Right;
+  relEnd := (FocusThread.CurrentRect.Left+FocusThread.CurrentRect.Right) / FocusThread.RootRect.Right;
+  result := (relStart<=relPos) and (relPos <= relEnd);
+end;
+
+procedure waitForReply(serial: TSdpoSerial);
+var startTime: int64;
+begin
+  startTime := DateTimeToUnix(now);
+  while (not (serial.DataAvailable)) and (not(startTime + 1 < DateTimeToUnix(now))) do begin  end;
+  while (serial.DataAvailable) do begin serial.ReadData end;
+end;
+
+procedure wakeSerial(serial: TSdpoSerial);
+begin
+  serial.WriteData('A');
+  waitForReply(serial);
+end;
 
 procedure FocusChanged();
-begin
+var buf : array[0..6] of char;
+  i: integer;
+const sleepDelay: integer = 1;
+begin  {
   writeln(Format('Focus is on %d @%d,%d size: %dx%d',[
     FocusThread.CurrentWindow,
     FocusThread.CurrentRect.Left,
@@ -23,13 +72,148 @@ begin
     FocusThread.CurrentRect.Right,
     FocusThread.CurrentRect.Bottom
   ]));
+        }
+  wakeSerial(serial);
+  serial.writeData('s' + ls_h + ls_l);
+  waitForReply(serial);
+  serial.writeData('l' + ll_h + ll_l);
+  waitForReply(serial);
+  serial.writeData('t' + lt_h + lt_l);
+  waitForReply(serial);
+  serial.writeData('r');
+  waitForReply(serial);
+
+  for i := 0 to l_light-1 do begin
+    buf[0] := 'd';
+
+    if (isLit(i, l_light)) then begin
+      buf[1] := char(fo_r_h);
+      buf[2] := char(fo_g_h);
+      buf[3] := char(fo_b_h);
+      buf[4] := char(fo_r_l);
+      buf[5] := char(fo_g_l);
+      buf[6] := char(fo_b_l);
+    end else
+    begin
+      buf[1] := char(ot_r_h);
+      buf[2] := char(ot_g_h);
+      buf[3] := char(ot_b_h);
+      buf[4] := char(ot_r_l);
+      buf[5] := char(ot_g_l);
+      buf[6] := char(ot_b_l);
+    end;
+
+
+    serial.WriteBuffer(buf, 7);
+    waitForReply(serial);
+  end;
+  serial.writeData('e');
+
+
+end;
+
+procedure TserialHandler.gotSerial(obj: TObject);
+begin
+  while serial.DataAvailable do begin
+    writeln(serial.ReadData);
+  end;
+end;
+
+procedure fatalError(err: string);
+begin
+  writeln('Fatal error: ' + err);
+  halt(1);
+end;
+
+function hexCharToEncInt(h: char): char;
+begin
+  case h of
+    '0': result := char($0);
+    '1': result := char($1);
+    '2': result := char($2);
+    '3': result := char($3);
+    '4': result := char($4);
+    '5': result := char($5);
+    '6': result := char($6);
+    '7': result := char($7);
+    '8': result := char($8);
+    '9': result := char($9);
+    'a', 'A': result := char($a);
+    'b', 'B': result := char($b);
+    'c', 'C': result := char($c);
+    'd', 'D': result := char($d);
+    'e', 'E': result := char($e);
+    'f', 'F': result := char($f);
+  else fatalError('Invalid character in hex string: ' + h);
+  end;
+
+  result := char(ord(result) or $40)
+
 end;
 
 begin
+
+  conf := TIniFile.Create(iniFileName);
+
+  portName := conf.ReadString ('connection', 'port', '/dev/ttyUSB0');
+
+  l_skip   := conf.ReadInteger('leds', 'skip', 0);
+  l_light  := conf.ReadInteger('leds', 'use', 30);
+  l_tot    := conf.ReadInteger('leds', 'total', 30);
+
+  focusColour := conf.ReadString ('colours', 'focus', '00ff00');
+  otherColour := conf.ReadString ('colours', 'other', '000000');
+
+  if l_skip > 255 then fatalError('Cannot skip this many LEDs, max is 255');
+  if l_light > 255 then fatalError('Cannot use this many LEDs, max is 255');
+  if l_tot > 255 then fatalError('Maximum supported strip length is 255');
+
+  if length(focusColour) <> 6 then fatalError('Invalid focus colour. ' +
+                                                     'Expected six hex digits');
+  if length(otherColour) <> 6 then fatalError('Invalid other colour. ' +
+                                                     'Expected six hex digits');
+
+  ls_l := char($40 or  (l_skip  and  $f)       );
+  ls_h := char($40 or ((l_skip  and $f0) shr 4));
+
+  ll_l := char($40 or  (l_light and  $f)       );
+  ll_h := char($40 or ((l_light and $f0) shr 4));
+
+  lt_l := char($40 or  (l_tot   and  $f)       );
+  lt_h := char($40 or ((l_tot   and $f0) shr 4));
+
+  fo_r_h := hexCharToEncInt(focusColour[1]);
+  fo_r_l := hexCharToEncInt(focusColour[2]);
+  fo_g_h := hexCharToEncInt(focusColour[3]);
+  fo_g_l := hexCharToEncInt(focusColour[4]);
+  fo_b_h := hexCharToEncInt(focusColour[5]);
+  fo_b_l := hexCharToEncInt(focusColour[6]);
+
+  ot_r_h := hexCharToEncInt(otherColour[1]);
+  ot_r_l := hexCharToEncInt(otherColour[2]);
+  ot_g_h := hexCharToEncInt(otherColour[3]);
+  ot_g_l := hexCharToEncInt(otherColour[4]);
+  ot_b_h := hexCharToEncInt(otherColour[5]);
+  ot_b_l := hexCharToEncInt(otherColour[6]);
+
+  serial := TSdpoSerial.Create(nil);
+  serial.BaudRate:=br115200;
+  serial.StopBits:=sbOne;
+  serial.DataBits:=db8bits;
+  serial.Parity:=pNone;
+  serial.FlowControl:=fcHardware;
+  serial.Device:=portName;
+  serialHandler := TserialHandler.Create;
+  serial.OnRxData:=@serialHandler.gotSerial;
+  serial.Active:=true;
+
   XDisplay := XOpenDisplay(nil);
   if not (XDisplay = nil) then begin
     FocusThread := TXFocusListenerThread.Create(XDisplay, @FocusChanged);
-    readln();
+    while (true) do begin
+      readln();
+      FocusThread.interrupt;
+    end;
   end else
   begin
     writeln('Unable to open X display');
--- xfocuslistenerthread.pas
+++ xfocuslistenerthread.pas
@@ -5,8 +5,8 @@ unit XFocusListenerThread;
 interface
 
 uses
-  Classes, SysUtils,
-  xlib, x, ctypes, BaseUnix,Unix;
+  Classes,
+  xlib, x, ctypes, cmem, BaseUnix, Unix, pipes, math;
 
 type
   TFocusNotifyEvent = procedure;
@@ -17,10 +17,10 @@ type
       geom: TRect;
       rootgeom: TRect;
       FOnFocusNotify: TFocusNotifyEvent;
-      pipi, pipo: Text;
+      pipi, pipo: longint;
+      InterruptInStream: TInputPipeStream;
+      InterruptOutStream: TOutputPipeStream;
       procedure WaitForXEvent;
-      function XErrorIgnoreBadWindow(EXDisplay: PDisplay;
-                                                ErrorEvent: PXErrorEvent): cint;
     public
       procedure updateFocusedWindowData;
       property XDisplay: PDisplay write FXDisplay;
@@ -31,28 +31,62 @@ type
       procedure Execute; override;
       constructor Create(_XDisplay: PDisplay;
                   _OnFocusNotify: TFocusNotifyEvent);
+      procedure interrupt;
   end;
+
+var
+  focusThreadInstance: TXFocusListenerThread = nil;
 
 implementation
 
-function TXFocusListenerThread.XErrorIgnoreBadWindow(EXDisplay: PDisplay;
-            ErrorEvent: PXErrorEvent): cint;
+procedure TXFocusListenerThread.interrupt;
 begin
-  Writeln(pipo,''); // Interrupt the thread to trigger another update
-  result := 0;
+  // Interrupt the thread to trigger another update
+  InterruptOutStream.WriteAnsiString('#');
+end;
+
+function XErrorIgnoreBadWindow(EXDisplay: PDisplay;
+            ErrorEvent: PXErrorEvent): cint;
+var
+  buf: PChar;
+begin
+  // If this is a BadWidow error then ignore it
+  if ErrorEvent^.error_code = BadWindow then begin
+  end else
+  begin
+    // For any other error, print the message and die
+    buf := malloc(256);
+    XGetErrorText(EXDisplay, ErrorEvent^.error_code, buf, 255);
+    writeln('Unexpected X error: ', buf);
+    free(buf);
+    halt(1);
+  end;
 end;
 
 // Constructor that sets some fields and starts the thread
 constructor TXFocusListenerThread.Create(_XDisplay: PDisplay;
   _OnFocusNotify: TFocusNotifyEvent);
 begin
+  // To get around callback issues, this unit contains a reference to an
+  // instance of itself. This means there can only be one instance
+  if focusThreadInstance <> nil then
+     exit;
+  // Init fields
   FXDisplay := _XDisplay;
   FOnFocusNotify := _OnFocusNotify;
-  if assignpipe(pipi,pipo) <> 0 then
+  // Create a pipe for interrupting the thread if needed.
+  if assignpipe(pipi,pipo) <> 0 then begin
     Writeln('Error assigning pipes !');
-    exit(1);
-  XSetErrorHandler(TXErrorHandler(@this.XErrorIgnoreBadWindow));
+    exit;
+  end;
+  InterruptInStream := TInputPipeStream.create(pipi);
+  InterruptOutStream := TOutputPipeStream.create(pipo);
+  // Use our custom error handler that ignores BadWindow.
+  XSetErrorHandler(TXErrorHandler(@XErrorIgnoreBadWindow));
+  // Parent init
   inherited create(False);
+  // All done, keep a reference to ourself handy in this unit.
+  focusThreadInstance := self;
 end;
 
 // Get the currently focused window ID, its geometry and the root window
@@ -70,41 +104,60 @@ var
    children: ^TWindow;
    nchildren: cint;
    return_to: cint;
+
+   didError: boolean;
 begin
+
+  didError := false;
 
   // Get the currently focused window into a local variable (not the field yet)
   XGetInputFocus(FXDisplay, @wfocus, @return_to);
   // WFocus now contains a window id for a window that is focused but might not
-  // be a top level window
+  // be a top level window, walk up the tree until we find a top level window
+  if wfocus <> None then begin
+    repeat
+      // Get parent into wparent
+      if XQueryTree(FXDisplay, wfocus, @wroot, @wparent, @children, @nchildren)
+                                                         <> 0 then begin
+        // don't care about the children, free them
+        if nchildren > 0 then
+            XFree(children);
+        // If the parent of this window is the root window then this is a top
+        // level
+        if wparent = wroot then begin
+          break;
+        end else
+          // Not yet found top level, check parent
+          wfocus := wparent;
+      end else
+      begin
+        // If there was a problem getting the parent, mark the error and break
+        didError := True;
+        break;
+      end;
+    until false;
 
-  // Walk up the tree until we find a top level window
-  repeat
-    // Get parent into wparent
-    XQueryTree(FXDisplay, wfocus, @wroot, @wparent, @children, @nchildren);
-    // don't care about the children, free them
-    XFree(children);
-    // If the parent of this window is the root window then this is a top level
-    if wparent = wroot then begin
-      break;
+    if not didError then begin
+      // We have a top level window, update the current window ID field
+      FCurrentWindow := wfocus;
+      // Get the gemoetry
+      XGetGeometry(FXDisplay, wfocus, @wroot, @x, @y, @width, @height,
+                              @border_width, @depth);
+      geom := Rect(x,y, width, height);
+
+      // Also get the geometry for the root window
+      XGetGeometry(FXDisplay, wroot, @wroot, @x, @y, @width, @height,
+                              @border_width, @depth);
+      rootgeom := Rect(x,y, width, height);
+
+      //If the callback is set, call it
+      if not (FOnFocusNotify = nil) then FOnFocusNotify();
     end else
-      // Not yet found top level, check parent
-      wfocus := wparent;
-  until false;
-
-  // We have a top level window, update the current window ID field
-  FCurrentWindow := wfocus;
-  // Get the gemoetry
-  XGetGeometry(FXDisplay, wfocus, @wroot, @x, @y, @width, @height,
-                          @border_width, @depth);
-  geom := Rect(x,y, width, height);
-
-  // Also get the geometry for the root window
-  XGetGeometry(FXDisplay, wroot, @wroot, @x, @y, @width, @height,
-                          @border_width, @depth);
-  rootgeom := Rect(x,y, width, height);
-
-  //If the callback is set, call it
-  if not (FOnFocusNotify = nil) then FOnFocusNotify();
+    begin
+      // If there was an error, set the curent window to 0
+      FCurrentWindow:=0;
+    end;
+  end;
 end;
 
 // Wait for an X event to be available, when it is, discard it and return
@@ -112,21 +165,38 @@ end;
 procedure TXFocusListenerThread.WaitForXEvent;
 var
   nextxevent: TXEvent;
-  FDS : FDSet;
+  FDS : Tfdset;
   XFD : cint;
 begin
   // Select the 'FocusChange' and 'StructureNotify' events for the focused
   // window
   XSelectInput(FXDisplay, FCurrentWindow,
                           FocusChangeMask or StructureNotifyMask);
-  // Wait for the next event on that window or for an interrupt from the pipe
-  // Create a descriptor set with the X event queue and the interrupt pipe
-  FD_Zero (FDS);
-  FD_Set (pipi,FDS);
-  XFD := ConnectionNumber(FXDisplay);
-  Select (1,@FDS,nil,nil,nil);
-  while(XPending(FXDisplay))
+  // Wait for the next event on that window or for an interrupt from the pipe.
+  // To do this, first we create a descriptor set with the X event queue and
+  // the interrupt pipe.
+  fpFD_Zero (FDS);                    // New file descriptor set
+  XFlush(FXDisplay);                  // flush
+  XFD := ConnectionNumber(FXDisplay); // Get the X event queue file descriptor
+  fpFD_Set (XFD,FDS);                 // Add the X event Q FD to the set
+  fpFD_Set (pipi,FDS);                // Add the interrupt pipe FD to the set
+  // select on the file handles for X event queue and our internal interrupt
+  // pipe. This blocks until one of them has data to read.
+  fpSelect (max(pipi, XFD)+1,@FDS,nil,nil,nil);
+  // As soon as there is something to do, we continue here...
+  while (XPending(FXDisplay) > 0) do begin
+    // If we continued because there was an X event, we deal with it here.
     XNextEvent(FXDisplay, @nextxevent);
+  end;
+
+  if InterruptInStream.NumBytesAvailable > 0 then begin
+    // If we continued because there was an interrupt, we dea with it here
+    writeln('Interrupt');
+    //Interrupt fired, clear up waiting data to mark interrupt as complete
+    while (InterruptInStream.NumBytesAvailable > 0) do
+      InterruptInStream.ReadAnsiString;
+  end;
+
   // Deselect all events for this window
   XSelectInput(FXDisplay, FCurrentWindow, NoEventMask);
 end;
